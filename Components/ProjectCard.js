deb6b0b6c0a0654fa9e7acd26d9ef170eed15aa8
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-07-01 09:39:05 +0100

    Update ProjectCard.js

--- Components/ProjectCard.js
+++ Components/ProjectCard.js
@@ -5,9 +5,10 @@ const OGCard = (props) => {
         border: "#000613 solid 3px",
         borderRadius: "25px",
         boxShadow: "0 0 15px #222",
-        backgroundColor: "#FAEFEF"
+        backgroundColor: "#FAEFEF",
+        height: "calc(70vh-100px)"
       } 
-      let cardClass = "h-[calc(70vh-100px)] duration-300 w-[40vh] hover:w-[60vh]"
+      let cardClass = "duration-300 w-[40vh] hover:w-[60vh]"
     return(
         <div className={cardClass} style={cardStyle}>
             
