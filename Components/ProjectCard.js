2b1b604cb75d2b89c6f3cd72ead614217e5374be
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-07-03 11:45:30 +0100

    Trying to fix IOS issues

--- Components/ProjectCard.js
+++ Components/ProjectCard.js
@@ -30,14 +30,15 @@ const OGCard = (props) => {
                 backgroundColor: "#FAEFEF",
                 overflow: "hidden",
             }}>
-                <img src="close.png" onClick={()=>{
+                <a onClick={()=>{
                     setPopuped(0)
-                }}className="absolute h-10 w-10 m-[-2px] cursor-pointer bg-[#FAEFEF] p-2" style={{
+                }}><img src="close.png" className="absolute h-10 w-10 m-[-2px] cursor-pointer bg-[#FAEFEF] p-2" style={{
                     right: "0",
                     top: "0",
                     borderRadius: "1.5rem",
                     border: "#000613 solid 3px",
-                }}></img>
+                    zIndex: "8000"
+                }}></img></a>
                    <iframe src={props.link} width="100%" height="100%" frameBorder="0">Browser not compatible.</iframe>
             </div> : <div></div>}
             <a href={props.iframe ? undefined : props.link } className="cursor-pointer" target="_blank" rel="noopener noreferrer" onClick={(event, element) => {
--- Components/SkillCard.js
+++ Components/SkillCard.js
@@ -10,10 +10,18 @@ const SKillCard = (props) => {
       } 
       let cardClass = "min-w-[2.75rem] max-w-[16rem] p-2 h-11 flex flex-col justify-center items-center"
     return(
+        
         <>
         <div className="w-5 h-0" style={{
             border:"#000 solid 2px"
         }}></div>
+        <a href={props.iframe ? undefined : props.link } onClick={(event,element)=>{
+                if (event.target.src?.split('/')?.pop() == "close.png") {
+                    event.stopPropagation()
+                    return
+                }
+            if(props.iframe) setPopuped(1)
+            }} className="cursor-pointer" target="_blank" rel="noopener noreferrer" >
         <div className={cardClass} style={cardStyle}>
         {popuped == 1 ? <div className=
             {props.oOo ? 
@@ -30,20 +38,21 @@ const SKillCard = (props) => {
                 backgroundColor: "#FAEFEF",
                 overflow: "hidden",
             }}>
-                <img src="close.png" onClick={()=>{
+                <a onClick={()=>{
                     setPopuped(0)
-                }}className="absolute h-10 w-10 m-[-2px] cursor-pointer bg-[#FAEFEF] p-2" style={{
+                }}><img src="close.png"  className="absolute h-10 w-10 m-[-2px] cursor-pointer bg-[#FAEFEF] p-2" style={{
                     right: "0",
                     top: "0",
                     borderRadius: "1.5rem",
                     border: "#000613 solid 3px",
-                }}></img>
+                    zIndex:"8000"
+                }}></img></a>
                    <iframe src={props.link} width="100%" height="100%" frameBorder="0">Browser not compatible.</iframe>
             </div> : <div></div>}
-            <a href={props.iframe ? undefined : props.link } onClick={()=>{if(props.iframe) setPopuped(1)}} className="cursor-pointer" target="_blank" rel="noopener noreferrer" ><h1 className="font-black whitespace-nowrap">{props.text}</h1></a>
+            <h1 className="font-black whitespace-nowrap">{props.text}</h1>
           
         </div>
-        
+        </a>
         </>
         
     )
